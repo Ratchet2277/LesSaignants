57db11276a3bded4edbbe0b9eb2cecbec96bf049
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-07 10:04:52 +0200

    Création projet

---
+++ ExercicePairProgamming/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ExercicePairProgamming.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
