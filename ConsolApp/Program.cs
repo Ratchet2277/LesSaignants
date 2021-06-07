df91ec8386d1da9356143e4d12cfbd3a4fed0d52
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-07 10:18:07 +0200

    Ajout ConsolApp

---
+++ ConsolApp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ConsolApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
