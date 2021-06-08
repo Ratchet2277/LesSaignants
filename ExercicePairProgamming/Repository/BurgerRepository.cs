590b870a4a9835b23fc61461d909b36936da4632
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-08 10:41:36 +0200

    BurgerRepository

---
+++ ExercicePairProgamming/Repository/BurgerRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicePairProgamming.Dal;
+using ExercicePairProgamming.Models;
+
+namespace WebApplicationBurger.Repositories
+{
+    public class BurgerRepository : IBurgerRepository
+    {
+        private SaignantsContext context;
+
+        public BurgerRepository(SaignantsContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Burger> GetProducts()
+        {
+            return context.Burgers;
+        }
+
+        public void AddProduct(Burger burger)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Burger burger)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ ExercicePairProgamming/Repository/Interface.cs
@@ -0,0 +1,16 @@
+﻿using ExercicePairProgamming.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebApplicationBurger.Repositories
+{
+    public interface IBurgerRepository
+    {
+        public IQueryable<Product> GetBurger();
+        public void AddBurger(Burger burger);
+        public void DeleteBurger(Burger burger);
+    }
+}
