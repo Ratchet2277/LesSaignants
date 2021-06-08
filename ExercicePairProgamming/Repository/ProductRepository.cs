4015b20daee30712fcbd23248422a42bd0a123e9
GouyonAntoine <82935541+GouyonAntoine@users.noreply.github.com>
2021-06-08 10:24:58 +0200

    Ajout base repository

---
+++ ExercicePairProgamming/Repository/ProductRepository.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ExercicePairProgamming.Dal;
+using ExercicePairProgamming.Models;
+
+namespace WebApplicationBurger.Repositories
+{
+    public class ProductRepository : IProductRepository
+    {
+        private SaignantsContext context;
+
+        public ProductRepository(SaignantsContext context)
+        {
+            this.context = context;
+        }
+
+        public IQueryable<Product> GetProducts()
+        {
+            return context.Burgers;
+        }
+
+        public void AddProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteProduct(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
